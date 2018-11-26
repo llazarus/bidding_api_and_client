7233b9e5761d77e8e7dcd5fd670e7156b17f4d3c
Leland Lazarus <llazarus93@gmail.com>
2018-11-26 10:10:28 -0800

    Created Auction and Bid models

--- biddr_api/db/schema.rb
+++ biddr_api/db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_180404) do
+ActiveRecord::Schema.define(version: 2018_11_26_180937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "auctions", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.datetime "ends_on"
+    t.integer "reserve_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bids", force: :cascade do |t|
+    t.integer "bid"
+    t.bigint "auction_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["auction_id"], name: "index_bids_on_auction_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -25,4 +42,5 @@ ActiveRecord::Schema.define(version: 2018_11_26_180404) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "bids", "auctions"
 end
