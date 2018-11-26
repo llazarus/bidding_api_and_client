bd6b974f93ccfeb0b21cac42476736d441c04ce5
Leland Lazarus <llazarus93@gmail.com>
2018-11-26 11:12:38 -0800

    Fixed route to bid#create in Rails API. Cleaned up React app and filled requests.js file with relevant routes and actions

--- biddr_api/config/routes.rb
+++ biddr_api/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :auctions, only: [ :index, :show, :create, :destroy ] do
-        resources :bids, only: [ :destroy ]
+        resources :bids, only: [ :create ]
       end
       resource :sessions, only: [ :create, :destroy ]
       resources :users, only: [] do
